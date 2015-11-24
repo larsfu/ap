94513b7e866a31c67f3e091fc9a29f000e8140c1
Lars Funke <lars.funke@udo.edu>
2015-11-24 11:07:25 +0100

    auswertung tiel 2

--- 303/auswertung/plot.py
+++ 303/auswertung/plot.py
@@ -1,12 +1,13 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-x = np.linspace (0, 360, 1000)
+x_syn = np.linspace (0, 360, 1000)
 x,y =np.genfromtxt('auswertung/ohnerauschen.txt',unpack=True)
 plt.plot(x,y,'rx',label="Messwerte")
 plt.xlabel(r'$\phi /\mathrm{°}$')
 plt.ylabel(r'$U \ /\ \mathrm{V}$')
+plt.xlim(0,360)
 
-plt.plot (x,2*(2/np.pi)*22.8*np.cos(np.pi*x/180),'b-',label="Ausgangsspannung")
+plt.plot (x_syn,2*(2/np.pi)*22.8*np.cos(np.pi*x_syn/180),'b-',label="Ausgangsspannung")
 plt.legend(loc='best')
 plt.savefig('build/plot.pdf')
