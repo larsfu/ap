4779fc9a6f19dcc11948f1bdb7f1d2d5eb1b24a0
Lars Funke <lars.funke@udo.edu>
2015-11-24 10:59:30 +0100

    tweaks

--- 303/auswertung/plot2.py
+++ 303/auswertung/plot2.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
-x = np.linspace (0, 360)
+x = np.linspace (0, 360, 1000)
 x,y =np.genfromtxt('auswertung/mitrauschen.txt',unpack=True)
 plt.plot(x,y,'rx',label="Messwerte")
 plt.xlabel(r'$\phi /\mathrm{°}$')
 plt.ylabel(r'$U \ /\ \mathrm{V}$')
 plt.legend(loc='best')
-plt.plot (x,(2/np.pi)*22.8*np.cos(np.pi*x/180),'g-',label="Ausgangsspannung")
+plt.plot (x,2*(2/np.pi)*22.8*np.cos(np.pi*x/180),'g-',label="Ausgangsspannung")
 plt.legend(loc='best')
 plt.savefig('build/plot2.pdf')
