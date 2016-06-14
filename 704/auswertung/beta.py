019307710584ace3ea56f91b10556aa32f4e0f42
Lars Funke <lars.funke@udo.edu>
2016-06-14 12:02:47 +0200

    704 joah

---
+++ 704/auswertung/beta.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import uncertainties.unumpy as unp
+import uncertainties as unc
+
+def linregress(x, y):
+    assert len(x) == len(y)
+
+    x, y = np.array(x), np.array(y)
+
+    N = len(y)
+    Delta = N * np.sum(x**2) - (np.sum(x))**2
+
+    A = (N * np.sum(x * y) - np.sum(x) * np.sum(y)) / Delta
+    B = (np.sum(x**2) * np.sum(y) - np.sum(x) * np.sum(x * y)) / Delta
+
+    sigma_y = np.sqrt(np.sum((y - A * x - B)**2) / (N - 2))
+
+    A_error = sigma_y * np.sqrt(N / Delta)
+    B_error = sigma_y * np.sqrt(np.sum(x**2) / Delta)
+
+    return A, A_error, B, B_error
+
+Al = np.genfromtxt('daten/{}.txt'.format('Al'), unpack=True, dtype=object)
+
+d, delta_d, t, n = Al.astype(float)
+
+delta_d /= 1e6
+N_u = unc.ufloat(324/900, np.sqrt(324)/900)
+d = unp.uarray(d, delta_d) * 1e-6
+N = unp.uarray(n/t, np.sqrt(n)/t) - N_u
+
+slope1, std_a1, intercept1, std_b1 = linregress(unp.nominal_values(d[:4]), unp.nominal_values(N[:4]))
+slope2, std_a2, intercept2, std_b2 = linregress(unp.nominal_values(d[5:]), unp.nominal_values(N[5:]))
+
+print(std_a1, std_a2)
+
+D_max = (unc.ufloat(intercept2, std_b2)-unc.ufloat(intercept1, std_b1))/(unc.ufloat(slope1, std_a1) - unc.ufloat(slope2, std_a2))
+R_max = D_max * 2700 * 10
+print("D_max = {}µm, R_max = {}g/cm², E_max = {} MeV".format(D_max*1e6, R_max, 1.92*unp.sqrt(R_max**2 - 0.22*R_max)))
+#print(unp.nominal_values(d), N)
+sd = np.linspace(0, 0.5e-3)
+
+
+plt.ylim(-5, 50)
+plt.plot(1e6*sd, sd*slope2+intercept2, 'b-', label=r"Lineare Regression, $R > R_\text{max}$")
+plt.plot(1e6*sd, sd*slope1+intercept1, 'r-', label=r"Lineare Regression, $R < R_\text{max}$")
+plt.errorbar(1e6*unp.nominal_values(d[:4]), unp.nominal_values(N[:4]), fmt='rx', yerr=unp.std_devs(N[:4]), label=r"Messdaten, $R < R_\text{max}$")
+plt.errorbar(1e6*unp.nominal_values(d[5:]), unp.nominal_values(N[5:]), fmt='bx', yerr=unp.std_devs(N[5:]), label=r"Messdaten, $R > R_\text{max}$")
+plt.errorbar(1e6*unp.nominal_values(d[4:5]), unp.nominal_values(N[4:5]), fmt='kx', yerr=unp.std_devs(N[4:5]), label=r"Messdaten, unberücksichtigt")
+
+plt.legend(loc='best')
+plt.xlabel(r"$d/\si{\micro\meter}$")
+plt.ylabel(r"$N/\si{\per\second}$")
+plt.tight_layout(pad=0)
+plt.savefig("build/beta.pdf")
+plt.clf()
---
+++ 704/auswertung/plot.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import uncertainties.unumpy as unp
+import uncertainties as unc
+import scipy.constants as const
+import tools
+
+def fit(x, a, b):
+    return a * np.exp(b * x)
+
+def auswerten(name, d, n, t, z, V_mol, eps, raw):
+    d *= 1e-3
+    N = unp.uarray(n/t, np.sqrt(n)/t) - N_u
+
+    if name=="Cu":
+        tools.table((*raw, N), ("d/mm", "n", "(N-N_U)/\per\second"), "build/{}.tex".format(name), "Messdaten von {}.".format(name), "tab:daten{}".format(name), split=2)#, footer=r"$\Delta t = \SI{60}{s}$")#"(N-N_U)/\per\second"
+    else:
+        tools.table(raw, ("d/mm", "n", "\Delta t/s", "(N-N_U)/\per\second"), "build/{}.tex".format(name), "Messdaten von {}.".format(name), "tab:daten{}".format(name), split=2)
+    mu = z * const.N_A / V_mol * 2 * np.pi * (const.e**2 / (4 * np.pi * const.epsilon_0 * const.m_e * const.c**2))**2 * ((1+eps)/eps**2 * ((2 * (1+eps))/(1+2*eps) - 1/eps * np.log(1+2*eps)) + 1/(2*eps) * np.log(1+ 2*eps) - (1+ 3*eps)/(1+2*eps)**2)
+
+    params, pcov = curve_fit(fit, d, unp.nominal_values(N), sigma=unp.std_devs(N))
+    params_ = unc.correlated_values(params, pcov)
+    print("{}: N(0) = {}, µ = {}, µ_com = {}".format(name, params_[0], -params_[1], mu))
+
+    sd = np.linspace(0, .07, 1000)
+
+    valuesp = (fit(sd, *(unp.nominal_values(params_) + 10*unp.std_devs(params_)))).astype(float)
+    valuesm = (fit(sd, *(unp.nominal_values(params_) - 10*unp.std_devs(params_)))).astype(float)
+
+    #plt.xlim(0,7)
+    plt.xlabel(r"$d/\si{mm}$")
+    plt.ylabel(r"$N/\si{\per\second}$")
+    plt.plot(1e3*sd, fit(sd, *params), 'b-', label="Fit")
+    plt.fill_between(1e3*sd, valuesm, valuesp, facecolor='blue', alpha=0.125, edgecolor='none', label=r'$1\sigma$-Umgebung ($\times 10$)')
+    plt.errorbar(1e3*d, unp.nominal_values(N), yerr=unp.std_devs(N), fmt='rx', label="Messdaten")
+    plt.legend(loc='best')
+    plt.yscale('linear')
+    plt.tight_layout(pad=0)
+    plt.savefig("build/{}.pdf".format(name))
+    plt.yscale('log')
+    plt.savefig("build/{}_log.pdf".format(name))
+    plt.clf()
+
+Cu = np.genfromtxt('daten/{}.txt'.format('Cu'), unpack=True, dtype=object)
+Pb = np.genfromtxt('daten/{}.txt'.format('Pb'), unpack=True, dtype=object)
+
+d1, n1 = Cu.astype(float)
+t2, d2, n2 = Pb.astype(float)
+N_u = unc.ufloat(917/900, np.sqrt(917)/900)
+
+auswerten('Cu', d1, n1, 60, 29, 7.11e-6, 1.295, (Cu[0], Cu[1]))
+auswerten('Pb', d2, n2, t2, 82, 18.26e-6, 1.295, (Pb[1], Pb[2], Pb[0]))
